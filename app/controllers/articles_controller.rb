7f83d3639182504fe971f945be4cfd2268e12a5e
Isaac RF <IsaacRF239@gmail.com>
2023-10-02 10:11:21 +0200

    Add success / error display to article creation action and template
    - Also add flash to display successful creations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,11 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+        flash[:notice] = 'Article was successfully created'
+        redirect_to article_path(@article)
+    else
+        render 'new'
+    end
   end
 end
