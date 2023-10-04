a65a4c0015b53460ee0fc40d6e1d8c75e12d6c53
Isaac RF <IsaacRF239@gmail.com>
2023-10-04 11:13:42 +0200

    Refactor
    - Add partials for redundant pieces of view
    - Add private methods to generate variables used across multiple actions
    - Add a before_action to call the same method before calling certain actions to save lines

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,18 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: %i[show edit update destroy]
+
+  def show; end
 
   def index
     @articles = Article.all
   end
 
-  def new; end
+  def new
+    @article = Article.new
+  end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
         flash[:notice] = 'Article was successfully created'
         redirect_to article_path(@article)
@@ -19,13 +21,10 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = 'Article was successfully updated'
       redirect_to article_path(@article)
     else
@@ -34,12 +33,21 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
       flash[:notice] = 'Article was successfully deleted'
       redirect_to articles_path
     else
       render 'show'
     end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
